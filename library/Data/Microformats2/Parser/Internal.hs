ef4d54f5ef6fbde46ff0c256bf816bc41c645c2f
Val Packett <val@packett.cool>
2015-02-18 15:13:29 +0300

    more UnicodeSyntax

--- library/Data/Microformats2/Parser/Internal.hs
+++ library/Data/Microformats2/Parser/Internal.hs
@@ -17,22 +17,22 @@ documentRoot = view root
 if' ∷ Bool → Maybe a → Maybe a
 if' c x = if c then x else Nothing
 
-hasOneClass ∷ Applicative f => [String] → (Element → f Element) → Element → f Element
-hasOneClass ns = attributeSatisfies "class" $ \a -> Prelude.any (\x -> T.isInfixOf (T.pack x) a) ns
+hasOneClass ∷ Applicative f ⇒ [String] → (Element → f Element) → Element → f Element
+hasOneClass ns = attributeSatisfies "class" $ \a → Prelude.any (\x → T.isInfixOf (T.pack x) a) ns
 
-hasClass ∷ Applicative f => String → (Element → f Element) → Element → f Element
+hasClass ∷ Applicative f ⇒ String → (Element → f Element) → Element → f Element
 hasClass n = attributeSatisfies "class" $ T.isInfixOf . T.pack $ n
 
-valueParts ∷ Applicative f => (Element → f Element) → Element → f Element
+valueParts ∷ Applicative f ⇒ (Element → f Element) → Element → f Element
 valueParts = entire . hasOneClass ["value", "value-title"]
 
 _Content' ∷ Prism' Node Text
-_Content' = prism' NodeContent $ \s -> case s of
-  NodeContent c -> Just $ gsub [re|\s+|] (" " :: String) c
-  NodeElement e -> if' (e ^. name == "img") $ e ^. attribute "alt"
-  _ -> Nothing
+_Content' = prism' NodeContent $ \s → case s of
+  NodeContent c → Just $ gsub [re|\s+|] (" " :: String) c
+  NodeElement e → if' (e ^. name == "img") $ e ^. attribute "alt"
+  _ → Nothing
 
-allText ∷ Applicative f => (Text → f Text) → Element → f Element
+allText ∷ Applicative f ⇒ (Text → f Text) → Element → f Element
 allText = entire . nodes . traverse . _Content'
 
 extractValue ∷ Element → Maybe T.Text
@@ -54,6 +54,6 @@ data PropType = P | U | Dt | E
 
 extractProperty ∷ PropType → String → Element → Maybe T.Text
 extractProperty P n e' = do
-  e <- findProperty e' $ "p-" ++ n
+  e ← findProperty e' $ "p-" ++ n
   asum  [ if' (isJust $ e ^? valueParts) $ Just $ T.concat $ catMaybes $ e ^.. valueParts . to extractValuePart
         , extractValue e ]
