7ca0269ce3bc2139300299231860ea49a60186da
Val Packett <val@packett.cool>
2015-02-22 23:56:19 +0300

    add geo parser

--- test-suite/Data/Microformats2/Parser/InternalSpec.hs
+++ test-suite/Data/Microformats2/Parser/InternalSpec.hs
@@ -3,7 +3,7 @@
 module Data.Microformats2.Parser.InternalSpec (spec) where
 
 import           Test.Hspec
-import           Text.RawString.QQ
+import           TestCommon
 import           Text.HTML.DOM
 import           Text.XML.Lens (documentRoot)
 import           Data.Microformats2.Parser.Internal
@@ -15,79 +15,79 @@ spec = do
   describe "extractProperty" $ do
     it "parses p- properties" $ do
       let nm = extractProperty P "name" . documentRoot . parseLBS
-      nm [r|<span class="p-name">Hello Basic</span>|] `shouldBe` Just "Hello Basic"
-      nm [r|<abbr class="p-name" title="Hello Abbr">HA</abbr>|] `shouldBe` Just "Hello Abbr"
-      nm [r|<abbr class="p-name">HA</abbr>|] `shouldBe` Just "HA"
-      nm [r|<data class="p-name" value="Hello Data" />|] `shouldBe` Just "Hello Data"
-      nm [r|<input class="p-name" value="Hello Input" />|] `shouldBe` Just "Hello Input"
-      nm [r|<img class="p-name" alt="Hello Img" />|] `shouldBe` Just "Hello Img"
-      nm [r|<area class="p-name" alt="Hello Area" />|] `shouldBe` Just "Hello Area"
-      nm [r|<span class="p-name"> ignore <i class="value">Hello</i>  <img class="value" alt="ValuePattern" src="x.png"> </span>|] `shouldBe` Just "HelloValuePattern"
-      nm [r|<span class="p-name"> ignore <em class="value-title" title="Hello">Hi</em>  <span class="value">Value-Title</span></span>|] `shouldBe` Just "HelloValue-Title"
-      nm [r|<span class="p-name">  Hello <img alt="Span With Img" src="x.png"> </span>|] `shouldBe` Just "Hello Span With Img"
-      nm [r|<span class="p-name"> <span class="value"> 	Hello 	
+      nm [xml|<span class="p-name">Hello Basic</span>|] `shouldBe` Just "Hello Basic"
+      nm [xml|<abbr class="p-name" title="Hello Abbr">HA</abbr>|] `shouldBe` Just "Hello Abbr"
+      nm [xml|<abbr class="p-name">HA</abbr>|] `shouldBe` Just "HA"
+      nm [xml|<data class="p-name" value="Hello Data" />|] `shouldBe` Just "Hello Data"
+      nm [xml|<input class="p-name" value="Hello Input" />|] `shouldBe` Just "Hello Input"
+      nm [xml|<img class="p-name" alt="Hello Img" />|] `shouldBe` Just "Hello Img"
+      nm [xml|<area class="p-name" alt="Hello Area" />|] `shouldBe` Just "Hello Area"
+      nm [xml|<span class="p-name"> ignore <i class="value">Hello</i>  <img class="value" alt="ValuePattern" src="x.png"> </span>|] `shouldBe` Just "HelloValuePattern"
+      nm [xml|<span class="p-name"> ignore <em class="value-title" title="Hello">Hi</em>  <span class="value">Value-Title</span></span>|] `shouldBe` Just "HelloValue-Title"
+      nm [xml|<span class="p-name">  Hello <img alt="Span With Img" src="x.png"> </span>|] `shouldBe` Just "Hello Span With Img"
+      nm [xml|<span class="p-name"> <span class="value"> 	Hello 	
   <img alt="Span With Img" src="x.png"> </span> 	<em class="value-title" title="&& Value Title">nope</em> </span>|] `shouldBe` Just "Hello Span With Img&& Value Title"
 
     it "parses u- properties" $ do
       let ur = extractProperty U "url" . documentRoot . parseLBS
-      ur [r|<a class="u-url" href="/yo/a">link</a>|] `shouldBe` Just "/yo/a"
-      ur [r|<area class="u-url" href="/yo/area"/>|] `shouldBe` Just "/yo/area"
-      ur [r|<img class="u-url" src="/yo/img"/>|] `shouldBe` Just "/yo/img"
-      ur [r|<audio class="u-url" src="/yo/audio"/>|] `shouldBe` Just "/yo/audio"
-      ur [r|<video class="u-url" src="/yo/video"/>|] `shouldBe` Just "/yo/video"
-      ur [r|<source class="u-url" src="/yo/source"/>|] `shouldBe` Just "/yo/source"
-      ur [r|<span class="u-url"><b class=value>/yo</b><em class="value">/vcp</span>|] `shouldBe` Just "/yo/vcp"
-      ur [r|<abbr class="u-url" title="/yo/abbr"/>|] `shouldBe` Just "/yo/abbr"
-      ur [r|<data class="u-url" value="/yo/data"/>|] `shouldBe` Just "/yo/data"
-      ur [r|<input class="u-url" value="/yo/input"/>|] `shouldBe` Just "/yo/input"
-      ur [r|<span class="u-url">/yo/span</span>|] `shouldBe` Just "/yo/span"
+      ur [xml|<a class="u-url" href="/yo/a">link</a>|] `shouldBe` Just "/yo/a"
+      ur [xml|<area class="u-url" href="/yo/area"/>|] `shouldBe` Just "/yo/area"
+      ur [xml|<img class="u-url" src="/yo/img"/>|] `shouldBe` Just "/yo/img"
+      ur [xml|<audio class="u-url" src="/yo/audio"/>|] `shouldBe` Just "/yo/audio"
+      ur [xml|<video class="u-url" src="/yo/video"/>|] `shouldBe` Just "/yo/video"
+      ur [xml|<source class="u-url" src="/yo/source"/>|] `shouldBe` Just "/yo/source"
+      ur [xml|<span class="u-url"><b class=value>/yo</b><em class="value">/vcp</span>|] `shouldBe` Just "/yo/vcp"
+      ur [xml|<abbr class="u-url" title="/yo/abbr"/>|] `shouldBe` Just "/yo/abbr"
+      ur [xml|<data class="u-url" value="/yo/data"/>|] `shouldBe` Just "/yo/data"
+      ur [xml|<input class="u-url" value="/yo/input"/>|] `shouldBe` Just "/yo/input"
+      ur [xml|<span class="u-url">/yo/span</span>|] `shouldBe` Just "/yo/span"
 
     it "parses u- properties" $ do
       let dt = extractProperty Dt "updated" . documentRoot . parseLBS
-      dt [r|<time class="dt-updated" datetime="ti.me">someday</time>|] `shouldBe` Just "ti.me"
-      dt [r|<ins class="dt-updated" datetime="i.ns">someday</ins>|] `shouldBe` Just "i.ns"
-      dt [r|<del class="dt-updated" datetime="d.el">someday</del>|] `shouldBe` Just "d.el"
-      dt [r|<abbr class="dt-updated" title="ab.br">AB</abbr>|] `shouldBe` Just "ab.br"
-      dt [r|<data class="dt-updated" value="da.ta"/>|] `shouldBe` Just "da.ta"
-      dt [r|<input class="dt-updated" value="i.np.ut"/>|] `shouldBe` Just "i.np.ut"
-      dt [r|<span class="dt-updated">
+      dt [xml|<time class="dt-updated" datetime="ti.me">someday</time>|] `shouldBe` Just "ti.me"
+      dt [xml|<ins class="dt-updated" datetime="i.ns">someday</ins>|] `shouldBe` Just "i.ns"
+      dt [xml|<del class="dt-updated" datetime="d.el">someday</del>|] `shouldBe` Just "d.el"
+      dt [xml|<abbr class="dt-updated" title="ab.br">AB</abbr>|] `shouldBe` Just "ab.br"
+      dt [xml|<data class="dt-updated" value="da.ta"/>|] `shouldBe` Just "da.ta"
+      dt [xml|<input class="dt-updated" value="i.np.ut"/>|] `shouldBe` Just "i.np.ut"
+      dt [xml|<span class="dt-updated">
             <abbr class="value" title="vcp">VCP</abbr>
             <time class="value" datetime="ti">TIME</time>
             <ins class="value" datetime="me">lol</time>
           </span>|] `shouldBe` Just "vcptime"
-      dt [r|<span class="dt-updated">date</span>|] `shouldBe` Just "date"
+      dt [xml|<span class="dt-updated">date</span>|] `shouldBe` Just "date"
 
     it "parses e- properties" $ do
       let ct = extractProperty E "content" . documentRoot . parseLBS
-      ct [r|<div class="e-content"><em>hello html</em>!</div>|] `shouldBe` Just "<em>hello html</em>!"
+      ct [xml|<div class="e-content"><em>hello html</em>!</div>|] `shouldBe` Just "<em>hello html</em>!"
 
   describe "implyProperty" $ do
     it "parses implied p-name" $ do
       let nm = implyProperty P "name" . documentRoot . parseLBS
-      nm [r|<img class="h-blah" alt="Hello Img!">|] `shouldBe` Just "Hello Img!"
-      nm [r|<abbr class="h-blah" title="Hello Abbr!">HA</abbr>|] `shouldBe` Just "Hello Abbr!"
-      nm [r|<p class="h-blah"><img alt="Hello Only Img!"></p>|] `shouldBe` Just "Hello Only Img!"
-      nm [r|<p class="h-blah"><img alt="DOING"><img alt="IT"><img alt="WRONG">Goodbye Img!</p>|] `shouldBe` Just "Goodbye Img!"
-      nm [r|<p class="h-blah"><abbr title="Hello Only Abbr!">HOA</abbr></p>|] `shouldBe` Just "Hello Only Abbr!"
-      nm [r|<p class="h-blah"><abbr title="DOING"/><abbr title="IT"/><abbr title="WRONG"/>Goodbye Abbr!</p>|] `shouldBe` Just "Goodbye Abbr!"
-      nm [r|<p class="h-blah"><em><img alt="Hello Only Nested Img!"></p>|] `shouldBe` Just "Hello Only Nested Img!"
-      nm [r|<p class="h-blah"><em><img alt="DOING"><img alt="WRONG">Goodbye Nested Img!</p>|] `shouldBe` Just "Goodbye Nested Img!"
-      nm [r|<p class="h-blah"><em><abbr title="Hello Only Nested Abbr!">HOA</abbr></p>|] `shouldBe` Just "Hello Only Nested Abbr!"
-      nm [r|<p class="h-blah"><em><abbr title="DOING"/><abbr title="WRONG"/>Goodbye Nested Abbr!</p>|] `shouldBe` Just "Goodbye Nested Abbr!"
-      nm [r|<p class="h-blah">Hello Text!</p>|] `shouldBe` Just "Hello Text!"
+      nm [xml|<img class="h-blah" alt="Hello Img!">|] `shouldBe` Just "Hello Img!"
+      nm [xml|<abbr class="h-blah" title="Hello Abbr!">HA</abbr>|] `shouldBe` Just "Hello Abbr!"
+      nm [xml|<p class="h-blah"><img alt="Hello Only Img!"></p>|] `shouldBe` Just "Hello Only Img!"
+      nm [xml|<p class="h-blah"><img alt="DOING"><img alt="IT"><img alt="WRONG">Goodbye Img!</p>|] `shouldBe` Just "Goodbye Img!"
+      nm [xml|<p class="h-blah"><abbr title="Hello Only Abbr!">HOA</abbr></p>|] `shouldBe` Just "Hello Only Abbr!"
+      nm [xml|<p class="h-blah"><abbr title="DOING"/><abbr title="IT"/><abbr title="WRONG"/>Goodbye Abbr!</p>|] `shouldBe` Just "Goodbye Abbr!"
+      nm [xml|<p class="h-blah"><em><img alt="Hello Only Nested Img!"></p>|] `shouldBe` Just "Hello Only Nested Img!"
+      nm [xml|<p class="h-blah"><em><img alt="DOING"><img alt="WRONG">Goodbye Nested Img!</p>|] `shouldBe` Just "Goodbye Nested Img!"
+      nm [xml|<p class="h-blah"><em><abbr title="Hello Only Nested Abbr!">HOA</abbr></p>|] `shouldBe` Just "Hello Only Nested Abbr!"
+      nm [xml|<p class="h-blah"><em><abbr title="DOING"/><abbr title="WRONG"/>Goodbye Nested Abbr!</p>|] `shouldBe` Just "Goodbye Nested Abbr!"
+      nm [xml|<p class="h-blah">Hello Text!</p>|] `shouldBe` Just "Hello Text!"
 
     it "parses implied u-photo" $ do
       let ph = implyProperty U "photo" . documentRoot . parseLBS
-      ph [r|<img class="h-blah" src="selfie.png">|] `shouldBe` Just "selfie.png"
-      ph [r|<object class="h-blah" data="art.svg">|] `shouldBe` Just "art.svg"
-      ph [r|<div class="h-blah"><img src="selfie.png"/><em>yo</em>|] `shouldBe` Just "selfie.png"
-      ph [r|<div class="h-blah"><object data="art.svg"/><em>yo</em>|] `shouldBe` Just "art.svg"
-      ph [r|<div class="h-blah"><p class="onlychild"><img src="selfie.png"/><em>yo</em>|] `shouldBe` Just "selfie.png"
-      ph [r|<div class="h-blah"><p class="onlychild"><object data="art.svg"/><em>yo</em>|] `shouldBe` Just "art.svg"
+      ph [xml|<img class="h-blah" src="selfie.png">|] `shouldBe` Just "selfie.png"
+      ph [xml|<object class="h-blah" data="art.svg">|] `shouldBe` Just "art.svg"
+      ph [xml|<div class="h-blah"><img src="selfie.png"/><em>yo</em>|] `shouldBe` Just "selfie.png"
+      ph [xml|<div class="h-blah"><object data="art.svg"/><em>yo</em>|] `shouldBe` Just "art.svg"
+      ph [xml|<div class="h-blah"><p class="onlychild"><img src="selfie.png"/><em>yo</em>|] `shouldBe` Just "selfie.png"
+      ph [xml|<div class="h-blah"><p class="onlychild"><object data="art.svg"/><em>yo</em>|] `shouldBe` Just "art.svg"
 
     it "parses implied u-url" $ do
       let ur = implyProperty U "url" . documentRoot . parseLBS
-      ur [r|<a class="h-blah" href="/hello/a">|] `shouldBe` Just "/hello/a"
-      ur [r|<area class="h-blah" href="/hello/area">|] `shouldBe` Just "/hello/area"
-      ur [r|<div class="h-blah"><em>what</em><a href="/hello/n/a">|] `shouldBe` Just "/hello/n/a"
-      ur [r|<div class="h-blah"><em>what</em><area href="/hello/n/area">|] `shouldBe` Just "/hello/n/area"
+      ur [xml|<a class="h-blah" href="/hello/a">|] `shouldBe` Just "/hello/a"
+      ur [xml|<area class="h-blah" href="/hello/area">|] `shouldBe` Just "/hello/area"
+      ur [xml|<div class="h-blah"><em>what</em><a href="/hello/n/a">|] `shouldBe` Just "/hello/n/a"
+      ur [xml|<div class="h-blah"><em>what</em><area href="/hello/n/area">|] `shouldBe` Just "/hello/n/area"
--- test-suite/Data/Microformats2/ParserSpec.hs
+++ test-suite/Data/Microformats2/ParserSpec.hs
@@ -3,7 +3,7 @@
 module Data.Microformats2.ParserSpec (spec) where
 
 import           Test.Hspec
-import           Text.RawString.QQ
+import           TestCommon
 import           Data.Default
 import           Data.Microformats2
 import           Data.Microformats2.Parser
@@ -17,7 +17,7 @@ spec = do
     let parseGeo' = parseGeo . documentRoot . parseLBS
 
     it "parses valid h-geo" $ do
-      parseGeo' [r|<div>
+      parseGeo' [xml|<div>
         <p class="h-geo">
           <span class="p-latitude">37.33168</span>
           <span class="p-longitude">-122.03016</span>
@@ -30,7 +30,28 @@ spec = do
                           , def { geoLatitude = Just 1.2345 } ]
 
     it "ignores invalid properties" $ do
-      parseGeo' [r|<p class="h-geo">
+      parseGeo' [xml|<p class="h-geo">
           <span class="p-latitude">HELLO WORLD!!</span>
           <span class="p-altitude">1.2345</span>
         </p>|] `shouldBe` [ def { geoAltitude = Just 1.2345 } ]
+
+  describe "parseAdr" $ do
+    let parseAdr' = parseAdr . documentRoot . parseLBS
+
+    it "parses valid h-adr" $ do
+      parseAdr' [xml|<div>
+          <article class="h-adr">
+            <span class="p-street-address">SA</span>
+            <p class="p-extended-address">EA</p>
+            <abbr class="p-post-office-box" title="PO">_</abbr>
+            <span class="p-locality">L</span>
+            <span class="p-region">R</span>
+            <span class="p-postal-code">PC</span>
+            <span class="p-country-name">C</span>
+            <span class="p-label">LB</span>
+          </article>
+        </div>|] `shouldBe` [ def { adrStreetAddress = Just "SA", adrExtendedAddress = Just "EA"
+                                  , adrPostOfficeBox = Just "PO", adrLocality = Just "L"
+                                  , adrRegion = Just "R", adrPostalCode = Just "PC"
+                                  , adrCountryName = Just "C", adrLabel = Just "LB"
+                                  } ]
