5ce8df390f840f233bba1bfa284b9e0ddbeef85e
Val Packett <val@packett.cool>
2015-02-24 22:50:41 +0300

    add embedded h-geo in h-adr parsing

--- library/Data/Microformats2/Parser.hs
+++ library/Data/Microformats2/Parser.hs
@@ -14,18 +14,21 @@ module Data.Microformats2.Parser (
 import           Data.Microformats2
 import           Data.Microformats2.Parser.Internal
 import           Data.Default
+import           Control.Applicative
 import           Text.HTML.DOM
 import           Text.XML.Lens
 
 mapC ∷ String → (Element → α) → Element → [α]
 mapC n p e = map p $ e ^.. entire . hasClass n
 
+extractGeo ∷ Element → Geo
+extractGeo e = def { geoLatitude  = extractPropertyR P "latitude"  e
+                   , geoLongitude = extractPropertyR P "longitude" e
+                   , geoAltitude  = extractPropertyR P "altitude"  e }
+
 -- | Parses all h-geo entries inside of an element.
 parseGeo ∷ Element → [Geo]
-parseGeo = mapC "h-geo" parse
-  where parse e = def { geoLatitude  = extractPropertyR P "latitude"  e
-                      , geoLongitude = extractPropertyR P "longitude" e
-                      , geoAltitude  = extractPropertyR P "altitude"  e }
+parseGeo = mapC "h-geo" extractGeo
 
 -- | Parses all h-adr entries inside of an element.
 parseAdr ∷ Element → [Adr]
@@ -38,4 +41,6 @@ parseAdr = mapC "h-adr" parse
                       , adrPostalCode      = extractPropertyL P "postal-code"      e
                       , adrCountryName     = extractPropertyL P "country-name"     e
                       , adrLabel           = extractPropertyL P "label"            e
-                      }
+                      , adrGeo             = filter (/= GeoGeo def) $
+                          (GeoGeo . extractGeo $ e) : (TextGeo <$> extractPropertyL P "geo" e)
+                                                      ++ (map GeoGeo . parseGeo $ e) }
