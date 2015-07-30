9716c6d3ccf87d564e523612584f67abe2a4a261
Val Packett <val@packett.cool>
2015-07-30 16:23:45 +0300

    separate find/parse functions

--- library/Data/Microformats2/Parser.hs
+++ library/Data/Microformats2/Parser.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
 module Data.Microformats2.Parser (
-  parseGeo
-, parseAdr
-, parseCard
+  module Data.Microformats2.Parser
 
   -- * HTML parsing stuff (from html-conduit, xml-lens)
 , documentRoot
@@ -19,22 +17,33 @@ import           Control.Applicative
 import           Text.HTML.DOM
 import           Text.XML.Lens
 
-mapC ∷ String → (Element → α) → Element → [α]
-mapC n p e = map p $ e ^.. entire . hasClass n
+findMicroformat ∷ String → Element → [Element]
+findMicroformat n e = e ^.. entire . hasClass n
 
 -- | Parses all h-geo entries inside of an element.
 parseGeo ∷ Element → [Geo]
-parseGeo = mapC "h-geo" extractGeo
+parseGeo = map extractGeo . findGeo
 
+-- | Finds all h-geo elements inside of an element.
+findGeo ∷ Element → [Element]
+findGeo = findMicroformat "h-geo"
+
+-- | Parses an element as h-geo.
 extractGeo ∷ Element → Geo
 extractGeo e = def { geoLatitude  = extractPropertyR P "latitude"  e
                    , geoLongitude = extractPropertyR P "longitude" e
                    , geoAltitude  = extractPropertyR P "altitude"  e }
 
+
 -- | Parses all h-adr entries inside of an element.
 parseAdr ∷ Element → [Adr]
-parseAdr = mapC "h-adr" extractAdr
+parseAdr = map extractAdr . findAdr
 
+-- | Finds all h-adr elements inside of an element.
+findAdr ∷ Element → [Element]
+findAdr = findMicroformat "h-adr"
+
+-- | Parses an element as h-adr.
 extractAdr ∷ Element → Adr
 extractAdr e = def { adrStreetAddress   = extractPropertyL P "street-address"   e
                    , adrExtendedAddress = extractPropertyL P "extended-address" e
@@ -49,10 +58,16 @@ extractAdr e = def { adrStreetAddress   = extractPropertyL P "street-address"   
                                               (TextGeo <$> extractPropertyL P "geo" e)
                                               ++ (map (GeoGeo . extractGeo) $ findPropertyMicroformat e "p-geo") }
 
+
 -- | Parses all h-card entries inside of an element.
 parseCard ∷ Element → [Card]
-parseCard = mapC "h-card" extractCard
+parseCard = map extractCard . findCard
 
+-- | Finds all h-card elements inside of an element.
+findCard ∷ Element → [Element]
+findCard = findMicroformat "h-card"
+
+-- | Parses an element as h-card.
 extractCard ∷ Element → Card
 extractCard e = def { cardName              = extractPropertyL P "name" e
                     , cardHonorificPrefix   = extractPropertyL P "honorific-prefix" e
@@ -84,5 +99,4 @@ extractCard e = def { cardName              = extractPropertyL P "name" e
                     , cardImpp              = extractPropertyL U "impp" e
                     , cardSex               = extractPropertyL P "sex" e
                     , cardGenderIdentity    = extractPropertyL P "gender-identity" e
-                    , cardAnniversary       = extractPropertyR Dt "anniversary" e
-                    }
+                    , cardAnniversary       = extractPropertyR Dt "anniversary" e }
